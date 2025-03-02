65e82dce96926c66d249decd3756ece4b9194273
Himanshu Jain <jainhimanshu@live.com>
2025-03-02 23:45:25 +0530

    episode-2

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+import React  from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div",{id:"parent"},
     [
         React.createElement("div",{id:"child"},
             [
-                React.createElement("h1",{},"I am an H1 Tag"),
+                React.createElement("h1",{},"I am Himanshu Jain"),
                 React.createElement("h2",{},"I am an H2 Tag")
             ]
         ),
